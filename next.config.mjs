67caf3b99ca919ca894560893e74a16dae3b1670
mohamad-tohidi <the.mohammad.tohidi@gmail.com>
2025-11-27 20:54:32 +0330

    fix the exporting process

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // enable static HTML export
+  output: "export",
+
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
